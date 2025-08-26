774478654bd4bafb0ac88c7507a5a9d6af4e6a2c
Balram Patidar <contact.twinai@gmail.com>
2025-08-26 19:52:56 +0530

    Remove metadata verification from layout and add manual meta tag in head

---
+++ app/head.tsx
@@ -0,0 +1,7 @@
+export default function Head() {
+    return (
+        <>
+            <meta name="google-site-verification" content="KK9iYEHTsTBteR4t5BWJbkVtNdq1604Re-041Y9JIeI" />
+        </>
+    );
+}
