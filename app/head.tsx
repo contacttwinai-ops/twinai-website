7545235396f20563bba2893a0493c760664f2371
Balram Patidar <contact.twinai@gmail.com>
2025-08-26 20:44:22 +0530

    Add sitemap generation file

--- app/head.tsx
+++
@@ -1,7 +0,0 @@
-export default function Head() {
-    return (
-        <>
-            <meta name="google-site-verification" content="KK9iYEHTsTBteR4t5BWJbkVtNdq1604Re-041Y9JIeI" />
-        </>
-    );
-}
